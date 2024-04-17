add2b3a3f8c4dd6b959b50fa82cc1e160d25d4ab
Robert Crawford <robbie.crawford999@gmail.com>
2024-04-17 14:31:08 +0100

    adds get-api-users and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -325,3 +325,22 @@ describe("DELETE /api/comments/comment_id", () => {
     })
   })
 })
+
+describe("GET /api/users", () => {
+  test("GET 200: should return a status code of 200 and respond with an array of users", () => {
+    return request(app)
+    .get("/api/users")
+    .expect(200)
+    .then(( {body} ) => {
+      const users = body.users
+      expect(users).toHaveLength(4)
+      users.forEach((user) => {
+        expect(user).toEqual(expect.objectContaining({
+          username: expect.any(String),
+          name: expect.any(String),
+          avatar_url: expect.any(String)
+        }))
+      })
+    })
+  })
+})
