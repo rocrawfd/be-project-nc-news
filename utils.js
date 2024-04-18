750fb827439ad33b8a321411494d2da3b5b4d0f5
Robert Crawford <robbie.crawford999@gmail.com>
2024-04-18 11:28:31 +0100

    adds topic-query functions in getArticles

--- utils.js
+++ utils.js
@@ -1,11 +1,12 @@
 const db = require("./db/connection")
+const format = require('pg-format')
 
 exports.doesArticleExist = (articleId) => {
     return db.query(`SELECT * FROM articles WHERE article_id = $1`, [articleId])
     .then(({rows}) => {
         if(rows.length === 0){
             return Promise.reject({
-            status: 404, msg: '404 - Article Not Found'
+            status: 404, msg: '404 - Not Found'
         })}
     })
 }
@@ -15,7 +16,50 @@ exports.doesCommentExist = (commentId) => {
     .then(({rows}) => {
         if(rows.length === 0){
             return Promise.reject({
-            status: 404, msg: '404 - Comment Not Found'
+            status: 404, msg: '404 - Not Found'
         })}
+    })
+}
+
+exports.doesTopicExist = (topic) => {
+    return db.query(`SELECT * FROM topics WHERE slug = $1`, [topic])
+    .then(({rows}) => {
+        if(topic !== undefined){
+            if(rows.length === 0){
+                return Promise.reject({
+                    status: 404,
+                    msg: '404 - Not Found'
+                })
+            }
+        }
+    })
+}
+
+exports.checkExists = (table, column, value) => {
+    const sqlString = format(`SELECT * FROM %I WHERE %I = $1`, table, column)
+    return db.query(sqlString, [value])
+    .then(({rows}) => {
+        if(rows.length === 0){
+            return Promise.reject({
+                status: 404, msg: '404 - Not Found'
+            })
+        }
+    })
+}
+
+   
+exports.checkTopicExists = (topic) => {
+    if(/\d/g.test(topic)){
+        return Promise.reject({
+            status: 400, msg: '400 - Bad Request'
+        })
+    }
+    return db.query(`SELECT * FROM topics WHERE slug = $1`, [topic])
+    .then(({rows}) => {
+        if(rows.length === 0){
+            return Promise.reject({
+                status: 404, msg: '404 - Not Found'
+            })
+        }
     })
 }
