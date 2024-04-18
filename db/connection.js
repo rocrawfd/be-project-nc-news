14450e16ed67b34d33dea2d5a4a5157a7e6bc0a6
Robert Crawford <robbie.crawford999@gmail.com>
2024-04-18 13:43:17 +0100

    updates connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
@@ -16,4 +15,4 @@ if(ENV === 'production'){
   config.max = 2
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
