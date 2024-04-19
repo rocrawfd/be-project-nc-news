b7632c6fde75fadba0f5dddc6cae611bb5c9f1ea
Robert Crawford <robbie.crawford999@gmail.com>
2024-04-19 14:37:52 +0100

    adds sort-by and order queries to getArticles

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -13,31 +13,33 @@ exports.getArticleById = (req, res, next) => {
 };
 
 
-exports.getArticles = (req, res, next) => {
-  const {topic} = req.query
-  let doesItExist = false
-  if(topic){
-    doesItExist = checkExists('topics', 'slug', topic)
-  }
-  return Promise.all( [fetchArticles(topic), doesItExist] )
-  .then((promises) => {
-    const articles = promises[0]
-    res.status(200).send({articles})
-  })
-  .catch((err) => {
-    next(err)
-  })
-};
 
 
 exports.patchArticle = (req, res, next) => {
   const {inc_votes} = req.body
   const {article_id} = req.params;
     return updateArticle(inc_votes, article_id)
-      .then((article) => {
+    .then((article) => {
         res.status(200).send({ article });
       })
-    .catch((err) => {
-      next(err);
-    });
-};
+      .catch((err) => {
+        next(err);
+      });
+    };
+    
+    
+    exports.getArticles = (req, res, next) => {
+      const {topic} = req.query
+      const {sort_by} = req.query
+      const {order} = req.query
+      let topicExists = false
+      if(topic) { topicExists = checkExists('topics', 'slug', topic) }
+      return Promise.all( [fetchArticles(topic, sort_by, order), topicExists] )
+      .then((promises) => {
+        const articles = promises[0]
+        res.status(200).send({articles})
+      })
+      .catch((err) => {
+        next(err)
+      })
+    };
--- utils.js
+++ utils.js
@@ -21,19 +21,6 @@ exports.doesCommentExist = (commentId) => {
     })
 }
 
-exports.doesTopicExist = (topic) => {
-    return db.query(`SELECT * FROM topics WHERE slug = $1`, [topic])
-    .then(({rows}) => {
-        if(topic !== undefined){
-            if(rows.length === 0){
-                return Promise.reject({
-                    status: 404,
-                    msg: '404 - Not Found'
-                })
-            }
-        }
-    })
-}
 
 exports.checkExists = (table, column, value) => {
     const sqlString = format(`SELECT * FROM %I WHERE %I = $1`, table, column)
